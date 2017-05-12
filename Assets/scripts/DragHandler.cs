6560d8e30f352268fb2247cfbb3a14006e6c44b2
Jeff <jeffmc196@gmail.com>
2017-05-13 00:54:01 +0900

    set up unity project, add drag and drop function

---
+++ Assets/scripts/DragHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	public static GameObject itemBeingDragged;
+	Vector3 startPos;
+	Transform startParent;
+
+	#region IBeginDragHandler implementation
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		itemBeingDragged = this.gameObject;
+		startPos = transform.position;
+		startParent = transform.parent;
+		GetComponent<CanvasGroup> ().blocksRaycasts = false;
+		Debug.Log ("On drag");
+		Debug.Log (itemBeingDragged.name );
+	}
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		Debug.Log ("dargging");
+		Debug.Log (Input.mousePosition);
+		transform.position = Input.mousePosition;
+	}
+
+	#endregion
+
+	#region IEndDragHandler implementation
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+		Debug.Log ("End drag");
+		itemBeingDragged = null;
+		GetComponent<CanvasGroup> ().blocksRaycasts = true;
+		if (transform.parent == startParent) {
+			transform.position = startPos;
+		}
+
+	}
+
+	#endregion
+}
