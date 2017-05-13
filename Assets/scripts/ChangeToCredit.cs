a5a61b6a2cd03af417242a1ce3484a7846c69094
Shepard1212 <yingzeyu0310@foxmail.com>
2017-05-13 15:31:34 +1200

    basic layout for initialmenu

---
+++ Assets/scripts/ChangeToCredit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeToCredit : MonoBehaviour
+{
+    // Use this for initialization  
+    void Start()
+    {
+
+        GameObject btnObj = GameObject.Find("Credit");
+        Button btn = btnObj.GetComponent<Button>();
+        btn.onClick.AddListener(delegate ()
+        {
+            this.GoNextScene(btnObj);
+        });
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+    }
+
+    public void GoNextScene(GameObject NScene)
+    {
+        Application.LoadLevel("Credit");
+    }
+}
