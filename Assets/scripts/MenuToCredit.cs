04d5c6f3d501ec3b1870a2cb8f890e5f76a89de2
Shepard1212 <yingzeyu0310@foxmail.com>
2017-05-17 17:24:25 +1200

    Updating class name

--- Assets/scripts/MenuToCredit.cs
+++ Assets/scripts/MenuToCredit.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ChangeToCredit : MonoBehaviour
+public class MenuToCredit : MonoBehaviour
 {
     // Use this for initialization  
     void Start()
