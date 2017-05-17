1d766226176e176e8d09bca07a6ca9248bd6330d
Shepard1212 <yingzeyu0310@foxmail.com>
2017-05-17 16:21:05 +1200

    Updating buttons and scripts

--- Assets/scripts/IndexToMenu.cs
+++ Assets/scripts/IndexToMenu.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class IndexToMenu : MonoBehaviour {
+public class IndexToMenu : MonoBehaviour
+{
+    // Use this for initialization  
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        GameObject btnObj = GameObject.Find("Back");
+        Button btn = btnObj.GetComponent<Button>();
+        btn.onClick.AddListener(delegate ()
+        {
+            this.GoNextScene(btnObj);
+        });
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+    }
+
+    public void GoNextScene(GameObject NScene)
+    {
+        Application.LoadLevel("InitialMenu");
+    }
 }
