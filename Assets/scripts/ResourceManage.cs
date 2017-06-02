bd281fab77bd640e3d95abd55f12b51f09e7e86e
Jeff Mu <jeffmc196@gmail.com>
2017-06-02 13:42:30 +0900

    Read slide image and text from resources

--- Assets/scripts/ResourceManage.cs
+++ Assets/scripts/ResourceManage.cs
@@ -8,16 +8,22 @@ public class ResourceManage : MonoBehaviour {
 	public Object[] loader;
 	public TextAsset[] games;
 	public TextAsset[] quizs;
+	public TextAsset[] theories;
+	public Sprite[] slides;
 	public int startIndex, endIndex, currentIndex;
 	public int gameIndex;
+	public int leactureStartIndex, lectureEndIndex;
 
 	// Use this for initialization
 	void Start () {
 		loadGame ();
 		loadQuiz ();
+		loadSilde ();
+		loadTheory ();
 		DontDestroyOnLoad (transform.gameObject);
 	}
 
+	//Load game data from resource/game
 	void loadGame(){
 		loader = Resources.LoadAll("game", typeof(TextAsset));
 		games = new TextAsset[loader.Length];
@@ -31,6 +37,7 @@ public class ResourceManage : MonoBehaviour {
 		return games [index];
 	}
 
+	//Load quiz data from resource/quiz
 	void loadQuiz(){
 		loader = Resources.LoadAll("quiz", typeof(TextAsset));
 		quizs = new TextAsset[loader.Length];
@@ -44,6 +51,36 @@ public class ResourceManage : MonoBehaviour {
 		return quizs;
 	}
 
+	//Load quiz data from resource/slide
+	void loadSilde(){
+		loader = Resources.LoadAll("slide", typeof(Sprite));
+		slides = new Sprite[loader.Length];
+
+		for(int i = 0; i < loader.Length; i++){
+			slides [i] = (Sprite)loader [i];
+		}
+	}
+
+	public Sprite[] getSlide(){
+		return slides;
+	}
+
+	//Load quiz data from resource/theroy
+	void loadTheory(){
+		loader = Resources.LoadAll("theory", typeof(TextAsset));
+		theories = new TextAsset[loader.Length];
+
+		for(int i = 0; i < loader.Length; i++){
+			theories [i] = (TextAsset)loader [i];
+		}
+	}
+
+	public TextAsset[] getTheory(){
+		return theories;
+	}
+		
+
+	//Set the quiz to be instantiate
 	public void setQuizStartIndex(int startIndex){
 		this.startIndex = startIndex;
 	}
@@ -52,6 +89,7 @@ public class ResourceManage : MonoBehaviour {
 		this.endIndex = endIndex;
 	}
 
+	//Set the game to be instantiate
 	public void setGameIndex(int gameIndex){
 		this.gameIndex = gameIndex;
 	}
---
+++ Assets/scripts/TheroyLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TheroyLoader : MonoBehaviour {
+	public GameObject theoryField;
+
+	// Use this for initialization
+	void Start () {
+		TextAsset[] theories = GameObject.Find ("ResourceManager").transform.GetComponent<ResourceManage> ().getTheory ();
+		foreach (TextAsset t in theories) {
+			GameObject tf = Instantiate (theoryField);
+			tf.transform.SetParent (transform);
+			tf.transform.Find ("Text").GetComponent<Text> ().text = t.text;
+		}
+
+	}
+
+}
